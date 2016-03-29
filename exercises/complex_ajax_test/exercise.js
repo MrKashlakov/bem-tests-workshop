0675e624121691ffc44a06dc9ba19001037feef1
Ilya Kashlakov <i.kashlakov@yandex.ru>
2016-03-29 21:58:21 +0300

    Complex ajax testing - excercise completed

--- exercises/complex_ajax_test/exercise.js
+++ exercises/complex_ajax_test/exercise.js
@@ -12,23 +12,23 @@ exercise.addVerifyProcessor(function (callback) {
 		testCases: [
 			{
 				enbCommandOption: constants.ENB_JS_SPECS_OPTION,
-				blockPath: path.join(constants.JS_SPECS_FOLDER, 'form'),
+				blockPath: path.join(constants.JS_SPECS_FOLDER, 'form_ajax'),
 				errorMessage: 'Во время сборки произошла ошибка'
 			}
 		]
 	};
 
-	common.runEnbTestCases(options).then(function (stdOut) {
+	common.runEnbTestCases(options).then(function () {
 		var checkOptions = {
 			percent: 100,
 			baseDir: exercise.workshopper.appDir,
-			testCasePath: 'common.blocks/form/form.js'
+			testCasePath: 'common.blocks/form/_ajax/form_ajax.js'
 		};
 		common.checkJsCoverage(checkOptions).then(function (result) {
 			console.log(format('Покрыто тестами %d% при минимально возможном покрытии 100%',
-					result.currentPercent));
-			result.passed ? callback(null, true) : exercise.emit('fail', 
-					'Процент покрытия не отвечает требованиям задания');
+				result.currentPercent));
+			result.passed ? callback(null, true) : exercise.emit('fail',
+				'Процент покрытия не отвечает требованиям задания');
 		}).fail(function (err) {
 			exercise.fail('fail', 'Во время проверки задания возникла ошибка');
 			console.log(err);
